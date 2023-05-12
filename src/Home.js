e071af270d6d61ae279effeb4256510b32a2f993
Cesar Sanchez <cesarsnchz13@gmail.com>
2023-05-11 21:46:11 -0500

    start fixture page

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 
 const ShowLiveFixtures = () => {
 	return (
-		<div className='card home-card'>
+		<div className='card text-center home-card'>
+			<h3 className='card-title'>Live Fixtures</h3>
 			<div className='card-body'>
 				<table className='table'>
 					<thead>
@@ -45,7 +46,8 @@ const ShowLiveFixtures = () => {
 
 const ShowTodaysFixtues = () => {
 	return (
-		<div className='card home-card'>
+		<div className='card text-center home-card'>
+			<h3 className='card-title'>Today's Fixtures</h3>
 			<div className='card-body'>
 				<table className='table'>
 					<thead>
@@ -85,14 +87,60 @@ const ShowTodaysFixtues = () => {
 		</div>
 	);
 };
-const ShowThisWeeksFixtures = () => {};
+const ShowThisWeeksFixtures = () => {
+	return (
+		<div className='card text-center home-card'>
+			<h3 className='card-title'>This Week's Fixtures</h3>
+			<div className='card-body'>
+				<table className='table'>
+					<thead>
+						<tr>
+							<th scope='col'></th>
+							<th scope='col'>Home</th>
+							<th scope='col'></th>
+							<th scope='col'>Away</th>
+							<th scope='col'></th>
+						</tr>
+					</thead>
+					<tbody className='table-group-divider'>
+						<tr>
+							<th></th>
+							<td>Real Madrid</td>
+							<td>v</td>
+							<td>FC Barcelona</td>
+							<td></td>
+						</tr>
+						<tr>
+							<th></th>
+							<td>Real Madrid</td>
+							<td>v</td>
+							<td>FC Barcelona</td>
+							<td></td>
+						</tr>
+						<tr>
+							<th></th>
+							<td>Real Madrid</td>
+							<td>v</td>
+							<td>FC Barcelona</td>
+							<td></td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
+};
 
 export default function Home() {
 	return (
 		<div id='home'>
-			<h1>Welcome to Soccer App!</h1>
+			<h1>Matchday Mixtures</h1>
 			<p>Display live games, upcoming fixtures, and more.</p>
-			<ShowLiveFixtures />
+			<div id='fixture-cards'>
+				<ShowLiveFixtures />
+				<ShowTodaysFixtues />
+				<ShowThisWeeksFixtures />
+			</div>
 		</div>
 	);
 }
