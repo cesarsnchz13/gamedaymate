1f0df98440a8c0fbf6b7a036c051bf39ab45fd65
Cesar Sanchez <cesarsnchz13@gmail.com>
2023-05-28 22:53:06 -0500

    relocate files

---
+++ src/components/Leagues.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Leagues = () => {
+	return (
+		<div>
+			<h1>Leagues</h1>
+			<p>
+				Here you can find information about different soccer leagues around the
+				world.
+			</p>
+			{/* Add additional content and components as needed */}
+		</div>
+	);
+};
+
+export default Leagues;
---
+++ src/components/Teams.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Teams = () => {
+	return (
+		<div>
+			<h1>Teams</h1>
+			<p>Explore and discover your favorite soccer teams.</p>
+			{/* Add additional content and components as needed */}
+		</div>
+	);
+};
+
+export default Teams;
