e39cc58ce887ceb93457abe5fd730fd6e2e812dd
Cesar Sanchez <cesarsnchz13@gmail.com>
2023-05-29 15:57:29 -0500

    get top league fixtures

--- src/services/api.js
+++ src/services/api.js
@@ -1,17 +1,18 @@
 import axios from 'axios';
 
-async function makeApiRequest(path, params = {}) {
-	const url = `${process.env.REACT_APP_API_BASE_URL}/${path}`;
-	console.log({ url });
-	const options = {
+async function makeApiRequest(version, path, params = {}) {
+	const url = `${process.env.REACT_APP_API_BASE_URL}/${version}/${path}`;
+
+	let options = {
 		method: 'GET',
 		url,
-		params,
 		headers: {
 			'X-RapidAPI-Key': process.env.REACT_APP_API_KEY,
 			'X-RapidAPI-Host': process.env.REACT_APP_API_HOST,
 		},
 	};
+
+	if (version === 'v3') options.params = params;
 
 	try {
 		const response = await axios.request(options);
@@ -22,14 +23,57 @@ async function makeApiRequest(path, params = {}) {
 	}
 }
 
-export async function getLeagueByName(leagueName) {
+export async function getLeagues() {
 	try {
-		const apiCall = await makeApiRequest('leagues', {
-			search: leagueName,
-		});
-		console.log(apiCall);
+		const apiCall = await makeApiRequest('v3', 'leagues');
 		return apiCall.response;
 	} catch (error) {
 		console.warn(error);
 	}
+}
+
+export async function getTeamsByLeague(leagueId) {
+	let requestPath = `teams/league/${leagueId}`;
+	try {
+		const apiCall = await makeApiRequest('v2', requestPath);
+		return apiCall.response;
+	} catch (error) {
+		console.warn(error);
+	}
+}
+
+export async function getFixturesByLeague(leagueId) {
+	try {
+		const apiCall = await makeApiRequest('v3', 'leagues', {
+			league: leagueId,
+			season: 2022,
+		});
+		return apiCall.response;
+	} catch (error) {
+		console.warn(error);
+	}
+}
+
+export async function getPremierLeagueFixtures() {
+	return getFixturesByLeague(39);
+}
+
+export async function getLaLigaFixtures() {
+	return getFixturesByLeague(140);
+}
+
+export async function getBundesligaFixtures() {
+	return getFixturesByLeague(78);
+}
+
+export async function getSerieAFixtures() {
+	return getFixturesByLeague(135);
+}
+
+export async function getChampionsLeagueFixtures() {
+	return getFixturesByLeague(2);
+}
+
+export async function getEuropaLeagueFixtures() {
+	return getFixturesByLeague(3);
 }
